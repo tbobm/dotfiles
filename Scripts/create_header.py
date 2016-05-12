808ab2ef36be44b23385ede414a91293a2b71686
Theo Massard <massar_t@etna-alternance.net>
2016-05-12 11:14:40 +0200

    up create headers

--- Scripts/create_header.py
+++ Scripts/create_header.py
@@ -9,9 +9,16 @@ def create_config(params=None):
     try:
         informations = {"email": params[0], "name": params[1], "firstname": params[2]}
         print("Your infos {}".format(informations))
+        return informations
     except IndexError:
         print("Missing informations, expected 3 ")
+        return False
 
 
 if __name__ == '__main__':
-    create_config(sys.argv[1:])
+    infos = create_config(sys.argv[1:])
+    config_path = os.path.expanduser('~') + "/.myheaderconfig.json"
+    print(config_path)
+    infos = False
+    if infos is not False:
+        config_path = os.expanduser('~')+'1'
