24e23e21b5a169a4165af11ba9d0e7cb343a5e35
Theo Massard <massar_t@etna-alternance.net>
2016-08-09 09:17:26 +0200

    Update my_emacs, update new_vm

--- new_vm.sh
+++ new_vm.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-apt-get update && apt-get upgrade && apt-get dist-upgrade && apt-get update
-apt-get install -y git-core ssh curl npm
+function up_to_date() {
+    apt-get update
+    apt-get upgrade
+    apt-get dist-upgrade
+    apt-get update
+}
+
+    apt-get install -y git-core ssh curl npm
