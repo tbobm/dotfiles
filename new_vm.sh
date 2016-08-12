be6842b865b115a07427ba003283cd7a0c881630
Theo Massard <massar_t@etna-alternance.net>
2016-08-12 14:34:10 +0200

    [UPD] Updated new_vm.sh

--- new_vm.sh
+++ new_vm.sh
@@ -1,10 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
+AGY="apt-get install -y"
+AG="apt-get"
+
+set -x
 function up_to_date() {
-    apt-get update
-    apt-get upgrade
-    apt-get dist-upgrade
-    apt-get update
+    $AG update
+    $AG upgrade -y
+    $AG dist-upgrade
+    $AG update
 }
 
-    apt-get install -y git-core ssh curl npm
+function get_required_installer() {
+    ./npm_glob_modules.sh
+#    bash Installs/install_python3.5.sh
+#    pip install -r about_me/pip_list.txt
+}
+
+function get_configuration() {
+    rm -rf ~/.emacs.d/
+    git clone --recursive http://github.com/massard-t/my_emacs ~/.emacs.d/
+    echo ./UserSettings/bash_profile > ~/.bashrc
+}
+
+function install_dependencies() {
+    $AGY git-core
+    $AGY ssh
+    $AGY curl
+    $AGY npm
+    $AGY python-pip
+    $AGY python-dev
+}
+
+function install_opt_dependencies() {
+    $AGY tree
+    $AGY tmux
+    $AGY emacs24-nox
+    $AGY make
+}
+
+function complete_install() {
+    up_to_date
+    install_dependencies
+    install_opt_dependencies
+    get_configuration
+    get_required_installer
+}
+
+case "$1" in
+    --complete) complete_install
+		;;
+esac
+set +x
