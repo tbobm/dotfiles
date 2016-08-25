e94ec21a4b872f3cdaaca799812535fd9e8d9eb0
massard-t <massar_t@etna-alternance.net>
2016-08-25 13:36:29 +0200

    [UPD]Updated docker compose installation (requires pip now)

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -qO- https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo -H pip install docker-compose
