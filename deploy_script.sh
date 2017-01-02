caa0f8635c48963b281b0ea88d42f678a638ea05
Theo Massard <massar_t@etna-alternance.net>
2017-01-02 15:28:31 +0100

    [UPD]

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
+GO="${GOROOT:/usr/local/go}"
+INSTALL="sudo apt-get install -y"
+SET_ALT="sudo update-alternatives"
+GO_URL="https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz"
+CONFIG_REPO="https://github.com/massard-t/MyConfiguration"
+
 
 mkdir -p usr/w/{work,scripts} bin etc/{MyConfiguration,clones}
 
-ssh-keygen 
+ssh-keygen
 
-cd etc/MyConfiguration/
+cd etc/MyConfiguration/ || exit 1
 
 # Installing git
-sudo apt-get install -y git git-core
+$INSTALL git git-core
 
 git init
-git remote add origin https://github.com/massard-t/MyConfiguration
+git remote add origin $CONFIG_REPO
 git fetch --all
 git checkout master
 
 # Linking to github's bashrc
-rm ~/.bashrc 
+rm ~/.bashrc
 
 ln -s ~/etc/MyConfiguration/UserSettings/bash_profile ~/.bashrc
 
-cd ~
+cd ~ || exit 1
 
 # Installing common dependencies
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip
+$INSTALL software-properties-common
+$INSTALL python-dev python-pip python3-dev python3-pip
 
 # Installing golang
-wget -O golang https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
-sudo tar -zxvf golang -C /usr/local/go
-mkdir -p ~/usr/w/work
+wget -O golang $GO_URL
+sudo tar -zxvf golang -C "$GO"
 
 # Installing firefox
-sudo apt-get install -y firefox
+$INSTALL firefox
 
 # Installing neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y neovim
+$INSTALL neovim
 
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
+$SET_ALT --install /usr/bin/vi vi /usr/bin/nvim 60
+$SET_ALT --config vi
+$SET_ALT --install /usr/bin/vim vim /usr/bin/nvim 60
+$SET_ALT --config vim
+$SET_ALT --install /usr/bin/editor editor /usr/bin/nvim 60
+$SET_ALT --config editor
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
