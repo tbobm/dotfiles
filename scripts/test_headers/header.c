861dd13bdc12742ed9fdb518dd7fe2349b19cac9
Theo Massard <massar_t@etna-alternance.net>
2017-03-26 16:18:33 +0200

    [UPD] rm

--- scripts/test_headers/header.c
+++
@@ -1,9 +0,0 @@
- *
-** Get_infos.c for projet_easter in /var/Projet_Easter/NPROJECT
-**
-** Made by GUEYE Osir
-** Login   <gueye_o@etna-alternance.net>
-**
-** Started on  Tue Apr 26 14:04:04 2016 GUEYE Osir
-** Last update Wed Apr 27 05:36:19 2016 GUEYE Osir
-*/
